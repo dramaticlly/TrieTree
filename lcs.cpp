3057e493cce63972d7eeef9099ec6a06b4b1a0d6
tangmaomao <steveiszhy@163.com>
2014-11-24 21:07:32 -0500

    read from files; add clean to make file

--- lcs.cpp
+++ lcs.cpp
@@ -6,17 +6,29 @@ using namespace std;
 
 //
 int main (int argc, char* argv[]) {
-  ofstream myfile;
+  fstream myfile;
   string line;
-
+  cout<<"Argument Num:"<<argc<<endl;
   if(argc < 1)          // we expect exact one argument
  {
          std::cerr << "Usage: " << argv[0] << " File Path " << std::endl;
          return -1;
  }
 
-  myfile.open (argv[0]);
-  cout << "successful open the file"<<endl;
+  myfile.open (argv[1]);  //argv[0] is ./lcs, not the first argument!!!
+  cout << "successful open the file. File name \""<<argv[1]<<"\""<<endl;
+  //if file is open, tehn read from the file
+  if (myfile.is_open())
+        {
+        while (getline (myfile,line))
+                {
+                        cout << line <<endl;
+                }
+        }
+  else
+        cout << "Unable to read from file"<<endl;
+
   myfile.close();
   return 0;
 }
+
