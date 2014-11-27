554a25ec8b9e4126863b9066093445b0e1b1facc
tangmaomao <steveiszhy@163.com>
2014-11-26 22:06:20 -0500

    add string hash function

--- lcs.cpp
+++ lcs.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <string>
+#include <tr1/functional>
 using namespace std;
 
 void printvector (vector<string> &tokens)
@@ -25,12 +27,13 @@ void print2dvector( vector< vector<string> >& tokens)
 //       //vector< vector<string> >::iterator iter = tokens.begin();
         //vector< vector<string> >::iterator endr = tokens.end();
 int main (int argc, char* argv[]) {
-  fstream myfile;
   int InputlineLoopCounter = 0;
+  fstream myfile;
   string line;
   string temp;
   vector< vector<string> > parsed_array;
   vector<string> parsed_element;
+  tr1::hash<string> strHash;
   cout<<"<	  Argument Numumber:"<<argc<<endl;
   if(argc < 1)          // we expect exact one argument
  {
@@ -46,14 +49,16 @@ int main (int argc, char* argv[]) {
         while (getline (myfile,line))
                 {
                 		parsed_element.clear(); //if I dont clear, it would include previous word, interesting
-                        cout << line <<endl;
-                        stringstream ss(line);
+                    cout << line <<endl;
+                    stringstream ss(line);
+                    cout<< "hashed key for this sentence is :"<<strHash(line)<<endl;
                         while (ss >> temp)
                                 parsed_element.push_back(temp);
-                        printvector(parsed_element);
-	                        parsed_array.push_back(parsed_element);  // A[2].push_back(5) is used to add string, not add vector
-	                        InputlineLoopCounter ++;
-	                        cout << "<	  InputlineLoopCounter: "<<InputlineLoopCounter<<endl;
+
+                    printvector(parsed_element);
+	                  parsed_array.push_back(parsed_element);  // A[2].push_back(5) is used to add string, not add vector
+	                  InputlineLoopCounter ++;
+	                  cout << "<	  InputlineLoopCounter: "<<InputlineLoopCounter<<endl;
                 }
              print2dvector(parsed_array);
         }
