dbb00bc893860c746fa58511d73edf7ab2c58510
tangmaomao <steveiszhy@163.com>
2014-11-29 14:24:28 -0500

    add chopped elelment vector array

--- lcs.cpp
+++ lcs.cpp
@@ -2,15 +2,15 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
-#include <string>
 #include <tr1/functional>
+#include <string>
 using namespace std;
 
 void printvector (vector<string> &tokens)
 {
-        //cout << "<	  Here's the Parsed Elements"<<endl;
+//      cout << "Here's the Parsed Elements"<<endl;
         for(int i = 0; i < tokens.size(); i++)
-                cout << "element["<< i << "]=" << tokens[i] << ",";
+                cout << "element["<< i << "]=" << tokens[i] <<endl ;
         cout<<endl;
 }
 
@@ -20,21 +20,21 @@ void print2dvector( vector< vector<string> >& tokens)
 
         for (int i = 0; i < tokens.size(); i++)
         {
-        	cout << "Array["<< i <<"]= "<<endl;
-        	printvector(tokens[i]);
+                cout << "Array["<< i <<"]= "<<endl;
+                printvector(tokens[i]);
         }
 }
-//       //vector< vector<string> >::iterator iter = tokens.begin();
-        //vector< vector<string> >::iterator endr = tokens.end();
+//
 int main (int argc, char* argv[]) {
-  int InputlineLoopCounter = 0;
   fstream myfile;
   string line;
   string temp;
+  tr1::hash<string> strHash;
+  int InputlineLoopCounter = 0;
   vector< vector<string> > parsed_array;
   vector<string> parsed_element;
-  tr1::hash<string> strHash;
-  cout<<"<	  Argument Numumber:"<<argc<<endl;
+  vector<string> choped_element;
+  cout<<"<       Argument Numumber:"<<argc<<endl;
   if(argc < 1)          // we expect exact one argument
  {
          std::cerr << "Usage: " << argv[0] << " File Path " << std::endl;
@@ -42,25 +42,33 @@ int main (int argc, char* argv[]) {
  }
 
   myfile.open (argv[1]);  //argv[0] is ./lcs, not the first argument!!!
-  cout << "<	Successful open the file!\n<	File name: \""<<argv[1]<<"\""<<endl;
+  cout << "<    Successful open the file!\n<    File name: \""<<argv[1]<<"\""<<endl;
   //if file is open, tehn read from the file
   if (myfile.is_open())
         {
         while (getline (myfile,line))
                 {
-                		parsed_element.clear(); //if I dont clear, it would include previous word, interesting
-                    cout << line <<endl;
-                    stringstream ss(line);
-                    cout<< "hashed key for this sentence is :"<<strHash(line)<<endl;
+                       cout << "<       InputlineLoopCounter: "<<InputlineLoopCounter<<endl;
+                        parsed_element.clear(); //if I dont clear, it would include previous word, interesting
+                        choped_element.clear();
+                        cout << line <<endl;
+                        choped_element.push_back(line);
+                        stringstream ss(line);
+                        cout<< "hashed key for this sentence is :"<<strHash(line)<<endl;
+                        string copystr (line);
                         while (ss >> temp)
-                                parsed_element.push_back(temp);
-
-                    printvector(parsed_element);
-	                  parsed_array.push_back(parsed_element);  // A[2].push_back(5) is used to add string, not add vector
-	                  InputlineLoopCounter ++;
-	                  cout << "<	  InputlineLoopCounter: "<<InputlineLoopCounter<<endl;
+                        {
+                        parsed_element.push_back(temp);
+                        copystr.erase(0,copystr.find_first_of(" \t")+1);
+                        choped_element.push_back(copystr);
+                        }
+                       choped_element.pop_back();
+                       printvector(parsed_element);
+                       printvector(choped_element);
+                       parsed_array.push_back(parsed_element);
+                       InputlineLoopCounter ++;
                 }
-             print2dvector(parsed_array);
+            // print2dvector(parsed_array);
         }
   else
         cout << "Unable to read from file"<<endl;
